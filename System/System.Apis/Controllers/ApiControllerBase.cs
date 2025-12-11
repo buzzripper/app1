3a38333d7d4e05b350a84809f49640d731d0b695
Andy Smith <asmith88@outlook.com>
2025-12-11 17:21:53 -0500

    wip

---
+++ System/System.Apis/Controllers/ApiControllerBase.cs
@@ -0,0 +1,60 @@
+﻿using Dyvenix.System.Shared.DTOs;
+using Dyvenix.System.Shared.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dyvenix.System.Apis.Controllers;
+
+[ApiController]
+public abstract class ApiControllerBase : ControllerBase
+{
+	protected async Task<IActionResult> Execute<T>(Func<Task<T>> action)
+	{
+		try
+		{
+			var result = await action();
+
+			return Ok(new ApiResponse
+			{
+				Success = true,
+				Data = result
+			});
+		}
+		catch (ValidationException ex)
+		{
+			return Ok(new ApiResponse
+			{
+				Success = false,
+				Error = new ApiError
+				{
+					Code = "VALIDATION",
+					Message = ex.Message
+				}
+			});
+		}
+		catch (NotFoundException ex)
+		{
+			return Ok(new ApiResponse
+			{
+				Success = false,
+				Error = new ApiError
+				{
+					Code = "NOT_FOUND",
+					Message = ex.Message
+				}
+			});
+		}
+		catch (Exception ex)
+		{
+			return Ok(new ApiResponse
+			{
+				Success = false,
+				Error = new ApiError
+				{
+					Code = "SERVER_ERROR",
+					Message = "An unexpected error occurred.",
+					Details = ex.Message
+				}
+			});
+		}
+	}
+}
---
+++ System/System.Shared/Exceptions/ValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dyvenix.System.Shared.Exceptions;
+
+public class ValidationException : Exception
+{
+	public ValidationException(string message)
+		: base(message)
+	{
+	}
+
+	public ValidationException(string message, Exception innerException)
+		: base(message, innerException)
+	{
+	}
+}
