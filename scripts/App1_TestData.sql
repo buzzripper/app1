10b09b2196a776bf673a52c82c642625e6a40859
buzzripper <asmith88@outlook.com>
2026-03-04 08:28:36 -0500

    Removed file with secrets

--- scripts/App1_TestData.sql
+++
@@ -1,8 +0,0 @@
-USE [App1]
-GO
-INSERT [dbo].[Client] ([Id], [Key], [Name], [BaseUrl]) VALUES (N'f7da3fd6-984e-4ccf-b6d1-9e86d039bd81', N'acme', N'Acme Industries', NULL)
-GO
-INSERT [dbo].[Client] ([Id], [Key], [Name], [BaseUrl]) VALUES (N'f7da3fd6-984e-4ccf-b6d1-9e86d039bd82', N'contoso', N'Contoso Warehousing', NULL)
-GO
-INSERT [dbo].[Client] ([Id], [Key], [Name], [BaseUrl]) VALUES (N'f7da3fd6-984e-4ccf-b6d1-9e86d039bd83', N'fabrikam', N'Fabrikam Manufacturing', NULL)
-GO
